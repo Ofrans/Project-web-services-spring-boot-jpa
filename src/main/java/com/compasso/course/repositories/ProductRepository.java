51c0d5692022e06db02d1d242fa3e1fbc4eb9385
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-04 16:03:35 -0300

    Product

---
+++ src/main/java/com/compasso/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.compasso.course.repositories;
+
+import com.compasso.course.entities.Category;
+import com.compasso.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
