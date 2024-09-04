091911015474b9bd5ff32e306bcd23452e551234
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-04 16:53:52 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ src/main/java/com/compasso/course/repositories/OrderItemRepository.java
@@ -0,0 +1,11 @@
+package com.compasso.course.repositories;
+
+import com.compasso.course.entities.Category;
+import com.compasso.course.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+
+
+}
