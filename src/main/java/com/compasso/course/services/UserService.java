98d01bcd457396e84c41fed7e52021600a1c13c3
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-05 13:32:43 -0300

    Exception handling - findById

--- src/main/java/com/compasso/course/services/UserService.java
+++ src/main/java/com/compasso/course/services/UserService.java
@@ -2,8 +2,10 @@ package com.compasso.course.services;
 
 import com.compasso.course.entities.User;
 import com.compasso.course.repositories.UserRepository;
+import com.compasso.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -21,7 +23,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj =  repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
