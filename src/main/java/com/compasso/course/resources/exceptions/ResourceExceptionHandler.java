13136140fb63a91a6f9b21f2dd5c27bb232ebfaa
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-09-05 13:44:30 -0300

    Exception handling - delete

--- src/main/java/com/compasso/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/compasso/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.compasso.course.resources.exceptions;
 
+import com.compasso.course.services.exceptions.DatabaseException;
 import com.compasso.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,6 +17,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> responseNotFound(ResourceNotFoundException ex, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError er = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestId());
+        return ResponseEntity.status(status).body(er);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException ex, HttpServletRequest request) {
+        String error = "Database Error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError er = new StandardError(Instant.now(), status.value(), error, ex.getMessage(), request.getRequestId());
         return ResponseEntity.status(status).body(er);
     }
